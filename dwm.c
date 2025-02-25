2a0516c79e04b3c5c7daf9bcb593c5be60d49827
Wesley Yang <wjyyang24@gmail.com>
2025-02-25 05:30:53 -0500

    added floating rule for firefox history window and fixed indent errors

--- dwm.c
+++ dwm.c
@@ -2578,13 +2578,13 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 void
 xinitvisual()
 {
-    XVisualInfo *infos;
+    	XVisualInfo *infos;
 	XRenderPictFormat *fmt;
 	int nitems;
 	int i;
 
 	XVisualInfo tpl = {
-        .screen = screen,
+        	.screen = screen,
 		.depth = 32,
 		.class = TrueColor
 	};
@@ -2592,21 +2592,21 @@ xinitvisual()
 
 	infos = XGetVisualInfo(dpy, masks, &tpl, &nitems);
 	visual = NULL;
-	for(i = 0; i < nitems; i ++) {
-        fmt = XRenderFindVisualFormat(dpy, infos[i].visual);
+	for (i = 0; i < nitems; i ++) {
+        	fmt = XRenderFindVisualFormat(dpy, infos[i].visual);
 		if (fmt->type == PictTypeDirect && fmt->direct.alphaMask) {
-            visual = infos[i].visual;
+            		visual = infos[i].visual;
 			depth = infos[i].depth;
 			cmap = XCreateColormap(dpy, root, visual, AllocNone);
 			useargb = 1;
 			break;
-        }
-    }
+        	}
+    	}
 
 	XFree(infos);
 
 	if (! visual) {
-        visual = DefaultVisual(dpy, screen);
+        	visual = DefaultVisual(dpy, screen);
 		depth = DefaultDepth(dpy, screen);
 		cmap = DefaultColormap(dpy, screen);
     }
