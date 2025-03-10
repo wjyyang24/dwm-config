29c125b0260bed55b41fbd9ade3595342586c899
Wesley Yang <wjyyang24@gmail.com>
2025-03-09 20:21:20 -0400

    added floating rule for spectacle

--- config.h
+++ config.h
@@ -48,6 +48,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "firefox",  "Places",   NULL,       0,       	    1,           -1 },
+	{ "spectacle",NULL,   	  NULL,       0,       	    1,           -1 },
 };
 
 /* layout(s) */
