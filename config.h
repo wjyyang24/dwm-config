756bba238e4640339c40c9aa379a03ff05264d2d
Wesley Yang <wjyyang24@gmail.com>
2025-03-13 00:01:28 -0400

    changed bar color and transparency

--- config.h
+++ config.h
@@ -16,12 +16,12 @@ static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
-static const char col_gray1[]       = "#111111";
+static const char col_gray1[]       = "#191724";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#ff4971"; /* pink */
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0xb3;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
